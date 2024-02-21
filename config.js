fcfb2712a2e3b603ead9e14be1dd607f4f478191
Tuan07minh <139234292+Tuan07minh@users.noreply.github.com>
2024-02-21 22:33:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,16 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `có lẽ tớ đã thích cậu ngay từ cái nhìn đầu tiên
-    phải chăng đến hôm nay nó không dừng lại ở chữ thích nữa : 3`,
+    introDesc: `Có lẽ lần gặp đầu tiên cũng không phải tình cờ nhưng tớ lại có cảm giác lạ :|_|`,
     btnIntro: 'hihi',
-    title: 'Phải chăng tớ đã yêu ngay từ cái nhìn đầu tiên và nụ cười ấy 😙',
-    desc: 'có thể cho tớ 1 cơ hội không dù chỉ là 1%',
-    btnYes: 'có em đồng ý <33',
+    title: 'Phải chăng tớ đã yêu cậu từ cái nhìn đầu tiên 😙',
+    desc: 'có lẽ tớ xin cậu 1 cơ hội thoi được không mặc dù hơi ích kỉ ',
+    btnYes: 'tớ đồng ý <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại chọn tớ <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu cũng phải yêu nhá  <33333333',
+    reply: 'Yêu thì yêu mà không yêu cũng phải yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Thứ 7 này đi chơi được hong công chúa.',
+    messDesc: 'chiều mai anh qua rủ xuống cantin chơi nha bé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'cám ơn ><
+    messLink: 'cảm ơn em nhiều
 }
